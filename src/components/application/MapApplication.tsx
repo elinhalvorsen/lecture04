e66d91d26ceee413b4a5a3bac04a5de42eb8d665
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-19 23:51:28 +0100

    Added css and the map

--- src/components/application/MapApplication.tsx
+++ src/components/application/MapApplication.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+useGeographic();
+const map = new Map({
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10, 59], zoom: 8 }),
+});
+
 const MapApplication = () => {
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
   return (
     <>
       <header>
         <h1>Lecture 4 Map</h1>
       </header>
       <nav></nav>
-      <main></main>
+      <div ref={mapRef}></div>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import MapApplication from "./components/application/MapApplication";
 
+import "./main.css";
+import "ol/ol.css";
+
 ReactDom.createRoot(document.getElementById("root")!).render(
   <MapApplication />,
 );

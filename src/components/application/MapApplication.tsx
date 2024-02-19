7037a585c0a4e06d883e47801c0721c5488d80ea
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-19 23:41:34 +0100

    Rendered the MapApplication component

---
+++ src/components/application/MapApplication.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const MapApplication = () => {
+  return (
+    <>
+      <header>
+        <h1>Lecture 4 Map</h1>
+      </header>
+      <nav></nav>
+      <main></main>
+    </>
+  );
+};
+export default MapApplication;
--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import MapApplication from "./components/application/MapApplication";
+
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <MapApplication />,
+);

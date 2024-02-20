5d060d8dfe619bb9bdb93b528c4194c474ba0770
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-20 15:49:45 +0100

    Added som styling

--- src/components/kommuner/KommunerAside.tsx
+++ src/components/kommuner/KommunerAside.tsx
@@ -1,14 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MapContext } from "../context/MapContext";
-const KommunerAside = () => {
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import { Feature } from "ol";
+type KommuneVectorLayer = VectorLayer<VectorSource<KommuneFeatures>>;
+
+interface KommuneProperties {
+  kommunenummer: string;
+  navn: Stedsnavn[];
+}
+interface Stedsnavn {
+  sprak: string;
+  navn: string;
+}
+
+type KommuneFeatures = {
+  getProperties(): KommuneProperties;
+} & Feature;
+const StedsNavn = (navn: Stedsnavn[]) => {
+  return navn.find((n) => n.sprak === "nor")?.navn;
+};
+const useKommuneFeatures = () => {
   const { layers } = useContext(MapContext);
 
-  const kommunerLayer = layers.find((l) => l.getClassName() === "kommuner");
+  const kommunerLayer = layers.find(
+    (l) => l.getClassName() === "kommuner",
+  ) as KommuneVectorLayer;
+
+  const [feature, setFeature] = useState<KommuneFeatures[]>();
+  const features = kommunerLayer?.getSource()?.getFeatures();
+
+  const handleSourceChange = () => {
+    setFeature(kommunerLayer?.getSource()?.getFeatures);
+  };
+
+  useEffect(() => {
+    kommunerLayer?.getSource()?.on("change", handleSourceChange);
+    return () => {
+      kommunerLayer?.getSource()?.un("change", handleSourceChange);
+    };
+  });
+  return { kommunerLayer, features };
+};
+
+const KommunerAside = () => {
+  const { features } = useKommuneFeatures();
+
   return (
     <>
-      <aside className={kommunerLayer ? "visable " : "hidden "}>
+      <aside className={features?.length ? "visable " : "hidden "}>
         <div>
           <h2>Kommuner</h2>
+          <ul>
+            {features?.map((k) => <li>{StedsNavn(k.getProperties().navn)}</li>)}
+          </ul>
         </div>
       </aside>
     </>
